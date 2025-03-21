4e48fbdb0fab1d1591d1db2cf73da9fa8bb644da
felipepaivavale <felipepaivavale@gmail.com>
2025-03-21 18:49:59 -0300

    configs do bd e create de migratees

--- mestrado/settings.py
+++ mestrado/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'mestrado.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mestrado_db',  
+        'USER': 'felip',    
+        'PASSWORD': '',  
+        'HOST': 'localhost', 
+        'PORT': '5432',   
     }
 }
 
